e7ad92611ccc40806b4ebb4d6b9c795f3e97a94f
ziabaub <ziabaub@gmail.com>
2019-10-12 16:14:05 +0300

    review

--- src/main/java/com/epam/uber/utils/PriceCalculator.java
+++ src/main/java/com/epam/uber/utils/PriceCalculator.java
@@ -3,7 +3,12 @@ package com.epam.uber.utils;
 import com.epam.uber.entity.order.Tariff;
 
 public class PriceCalculator {
-    public double calculatePrice(int from, int to, double rate) {
+
+    private PriceCalculator() {
+    }
+
+    public static double calculatePrice(int from, int to, Tariff tariff) {
+        System.out.println("hi");
         int zoneCount = Math.abs(from - to);
         double rate = tariff.getRate();
         return zoneCount * rate;
